987116b63e032b91b89693299154a0cd9d5880bc
dmitriy.kravets <Dmitriy.Kravets@wolterskluwer.com>
2021-11-07 22:19:03 +0300

    added reversestring package

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/kravetsd/go-katas/nbyear"
+	"github.com/kravetsd/go-katas/reversestrings"
 )
 
 func main() {
 	fmt.Println(nbyear.NbYear(1500, 5, 100, 5000))
+	fmt.Println(reversestrings("test"))
 }
